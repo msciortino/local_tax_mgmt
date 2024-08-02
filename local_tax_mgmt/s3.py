864fee6f42122526fe663f973ca6b19d39064cb0
Michel Sciortino <m.sciortino@reply.it>
2024-08-02 19:27:00 +0200

    Normalizing resources names

--- local_tax_mgmt/s3.py
+++ local_tax_mgmt/s3.py
@@ -1,6 +1,8 @@
 from constructs import Construct
 from aws_cdk import aws_s3 as s3
 
+from utils import normalize_resource_name
+
 
 class S3Construct(Construct):
     def __init__(self, scope: Construct, id: str, config: dict) -> None:
@@ -10,7 +12,9 @@ class S3Construct(Construct):
         for bucket_config in config["s3"]:
             bucket_name = bucket_config["bucket_name"]
             self._buckets[bucket_name] = s3.Bucket(
-                scope=self, id=bucket_name, **bucket_config
+                scope=self,
+                id=normalize_resource_name(bucket_name),
+                **bucket_config
             )
 
     @property
--- utils.py
+++ utils.py
@@ -5,4 +5,11 @@ def load_stage_config(stage: str) -> dict:
 
     config_path = f"config/{stage}.yml"
 
-    return benedict.from_yaml(config_path)
+    config = benedict.from_yaml(config_path)
+    config["stage"] = stage
+
+    return config
+
+def normalize_resource_name(name: str) -> str:
+    tokens= name.lower().split("-")
+    return "".join([token.capitalize() for token in tokens])
